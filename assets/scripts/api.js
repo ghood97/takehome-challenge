737bdc6c534fb001d90c404bdc18c2903938d3f3
Griffin Hood <griffinhood@verizon.net>
2019-11-18 14:08:43 -0500

    Make handlebars template for gifs
    Call handlebars template on gifs and display them on page

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -3,7 +3,7 @@ const API_KEY = 'G7UeXKtRLcPOfRfGRffJ5zH2LmLi2LE6'
 const search = (str) => {
   return $.ajax({
     method: 'GET',
-    url: `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${str}&limit=2`
+    url: `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${str}&limit=20`
   })
 }
 
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,6 +1,11 @@
+const gifTemplate = require('./templates/gif.handlebars')
 
 const onSearchSuccess = (response) => {
   console.log(response)
+  const gifHtml = gifTemplate({gifs: response.data})
+  $('.gif-container').html('')
+  $('.gif-container').append(gifHtml)
+  $('#query').val('')
 }
 
 const onSearchFailure = (response) => {
